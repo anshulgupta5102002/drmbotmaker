ccd9f1a6e9814a83e8827f0ca85cccbdd0bf4c6f
anshulgupta5102002 <85269205+anshulgupta5102002@users.noreply.github.com>
2021-09-27 20:16:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# widevine-L3-WEB-DL-Script
+This is a script created to WEB-DL L3 Widevine Content.
+
+Last Updated July 31, 2021.
+
+## Use the branch below if you aren't able to modify the code according to your needs
+
+<https://github.com/parnexcodes/widevine-L3-WEB-DL-Script/tree/method-2>
+
+Works well with .mpd files , for m3u8 please use n_m3u8 program (not included in this script).
+
+## How to use
+### Requirements
+* Python and pip
+* Widevine Key Guesser
+  * Download zip from https://github.com/parnexcodes/widevine-l3-guesser-modified
+  * Activate developer mode in Chrome Extensions
+  * Use "Load unpacked" to load the extracted extension folder
+* pyfiglet
+  * `pip install pyfiglet`
+* rich
+  * `pip install rich`
+
+### Get the keys
+Go to the protected stream you want to download. Activate the plugin (restart may be required after installing the extension) and download the extracted keys (keys.json).
+
+### Decode the video
+Download the widevine-L3-WEB-DL-Script from here (Code -> Download zip). Copy the downloaded keys.json file to the same folder.
+
+Run the downloader with `python webdl.py -o <name_without_extension>` from the folder you downloaded and extracted the script from.
+
+The script will look in the keys.json file, starting from the second element in the JSON array. If the script can't find any keys, either modify the script (line 27 and 31), or the keys.json. See <https://gist.github.com/parnexcodes/74fef2e33a2171031000a97c371a1a65> for examples for some common use cases.
+
+If there are multiple `mpd_url`s in the file and it isn't working, try changing them around. You can also change the `mpd_url` for a custom one if you have one.
+
+### Options
+-id and -s are optional (**id** to manually enter video and audio id from ytdl, **s** for subtitle url.). **Subtitle part is bugged right now**.
+
+## Report Issues
+
+Open Issue on Github if you get any problem.
---
+++ webdl.py
@@ -0,0 +1,93 @@
+import os
+import json
+import subprocess
+import argparse
+import sys
+import pyfiglet
+from rich import print
+from typing import DefaultDict
+
+title = pyfiglet.figlet_format('WEBDL Script', font='slant')
+print(f'[magenta]{title}[/magenta]')
+print("by parnex")
+print("Required files : yt-dlp.exe, mkvmerge.exe, mp4decrypt.exe, aria2c.exe\n")
+
+arguments = argparse.ArgumentParser()
+# arguments.add_argument("-m", "--video-link", dest="mpd", help="MPD url")
+arguments.add_argument("-o", '--output', dest="output", help="Specify output file name with no extension", required=True)
+arguments.add_argument("-id", dest="id", action='store_true', help="use if you want to manually enter video and audio id.")
+arguments.add_argument("-s", dest="subtitle", help="enter subtitle url")
+args = arguments.parse_args()
+
+with open("keys.json") as json_data:
+    config = json.load(json_data)
+    json_mpd_url = config[0]['mpd_url']
+    try:
+        keys = ""
+        for i in range(1, len(config)):
+            keys += f"--key {config[i]['kid']}:{config[i]['hex_key']} "
+    except:
+        keys = ""
+        for i in range(1, len(config)-1):
+            keys += f"--key {config[i]['kid']}:{config[i]['hex_key']} "
+
+currentFile = __file__
+realPath = os.path.realpath(currentFile)
+dirPath = os.path.dirname(realPath)
+dirName = os.path.basename(dirPath)
+
+youtubedlexe = dirPath + '/binaries/yt-dlp.exe'
+aria2cexe = dirPath + '/binaries/aria2c.exe'
+mp4decryptexe = dirPath + '/binaries/mp4decrypt_new.exe'
+mkvmergeexe = dirPath + '/binaries/mkvmerge.exe'
+SubtitleEditexe = dirPath + '/binaries/SubtitleEdit.exe'
+
+# mpdurl = str(args.mpd)
+output = str(args.output)
+subtitle = str(args.subtitle)
+
+if args.id:
+    print(f'Selected MPD : {json_mpd_url}\n')    
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-F', json_mpd_url])
+
+    vid_id = input("\nEnter Video ID : ")
+    audio_id = input("Enter Audio ID : ")
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', audio_id, '--fixup', 'never', json_mpd_url, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', vid_id, '--fixup', 'never', json_mpd_url, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])   
+
+else:
+    print(f'Selected MPD : {json_mpd_url}\n')
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'ba', '--fixup', 'never', json_mpd_url, '-o', 'encrypted.m4a', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])
+    subprocess.run([youtubedlexe, '-k', '--allow-unplayable-formats', '--no-check-certificate', '-f', 'bv', '--fixup', 'never', json_mpd_url, '-o', 'encrypted.mp4', '--external-downloader', aria2cexe, '--external-downloader-args', '-x 16 -s 16 -k 1M'])    
+
+
+print("\nDecrypting .....")
+subprocess.run(f'{mp4decryptexe} --show-progress {keys} encrypted.m4a decrypted.m4a', shell=True)
+subprocess.run(f'{mp4decryptexe} --show-progress {keys} encrypted.mp4 decrypted.mp4', shell=True)  
+
+if args.subtitle:
+    subprocess.run(f'{aria2cexe} {subtitle}', shell=True)
+    os.system('ren *.xml en.xml')
+    subprocess.run(f'{SubtitleEditexe} /convert en.xml srt', shell=True) 
+    print("Merging .....")
+    subprocess.run([mkvmergeexe, '--ui-language' ,'en', '--output', output +'.mkv', '--language', '0:eng', '--default-track', '0:yes', '--compression', '0:none', 'decrypted.mp4', '--language', '0:eng', '--default-track', '0:yes', '--compression' ,'0:none', 'decrypted.m4a','--language', '0:eng','--track-order', '0:0,1:0,2:0,3:0,4:0', 'en.srt'])
+    print("\nAll Done .....")
+else:
+    print("Merging .....")
+    subprocess.run([mkvmergeexe, '--ui-language' ,'en', '--output', output +'.mkv', '--language', '0:eng', '--default-track', '0:yes', '--compression', '0:none', 'decrypted.mp4', '--language', '0:eng', '--default-track', '0:yes', '--compression' ,'0:none', 'decrypted.m4a','--language', '0:eng','--track-order', '0:0,1:0,2:0,3:0,4:0'])
+    print("\nAll Done .....")    
+
+print("\nDo you want to delete the Encrypted Files : Press 1 for yes , 2 for no")
+delete_choice = int(input("Enter Response : "))
+
+if delete_choice == 1:
+    os.remove("encrypted.m4a")
+    os.remove("encrypted.mp4")
+    os.remove("decrypted.m4a")
+    os.remove("decrypted.mp4")
+    try:    
+        os.remove("en.srt")
+    except:
+        pass
+else:
+    pass
